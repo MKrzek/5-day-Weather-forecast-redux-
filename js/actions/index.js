0ec9ed95615ce3ed5529dc1d9a2f8669841588c4
Malgorzata <mkrzek@googlemail.com>
2017-11-01 15:18:49 +0000

    added axios

--- js/actions/index.js
+++ js/actions/index.js
@@ -1,3 +1,4 @@
+import axios from 'axios';
 const API_KEY = 'a390214491b6bc7a8ba0e0735659aec7';
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
@@ -5,7 +6,7 @@ export const FETCH_WEATHER= 'FETCH_WEATHER';
 
 export function fetchWeather(city){
     const url=`${ROOT_URL}&q=${city},us`;
-    const request=fetch(url);
+    const request=axios.get(url);
     console.log ('request', request);
     return {
         type: FETCH_WEATHER,

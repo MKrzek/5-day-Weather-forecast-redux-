3cf6b109901aa081faf0b7516f32786f14c5768d
Malgorzata <mkrzek@googlemail.com>
2017-11-04 17:47:25 +0000

    added avg/units

--- js/actions/index.js
+++ js/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const API_KEY = 'a390214491b6bc7a8ba0e0735659aec7';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?&units=metric&appid=${API_KEY}`;
 
 export const FETCH_WEATHER= 'FETCH_WEATHER';
 
